13a88be0241d44e2f22b64496b58ff208a039447
Ricardo Faria <ricardo.faria@outlook.com.br>
2016-12-28 22:28:06 -0200

    Issue #1 - Implementada visor e teclas

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,10 +4,6 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
 angular.module('calculadora', ['ngRoute', 'ui.bootstrap', 'toastr'])
   .constant('malarkey', malarkey)
@@ -15,8 +11,4 @@ angular.module('calculadora', ['ngRoute', 'ui.bootstrap', 'toastr'])
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
-  .controller('MainController', MainController)
-  .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective);
+  .controller('MainController', MainController);
